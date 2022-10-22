91b4d5bcc16b362ec15d7fab3bb2e4a195a36979
Scott Motte <mot@mot.la>
2022-10-21 22:41:51 -0700

    Add dotenv-vault

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
+require("dotenv-vault-core").config()
+
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
-    message: "Hello from root!",
+    message: `Hello ${process.env.HELLO}!`,
   });
 });
 
